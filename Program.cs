154ca27f00ef0c588cd02d90e1934e9b73dbb290
Yehor Tsapik <s32575@pjwstk.edu.pl>
2026-03-19 13:02:40 +0100

    add a class for testing the project

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using apbd_tut_03.Equipment;
+using apbd_tut_03.SystemUser;
+
+namespace apbd_tut_03;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var user1 = new Employee("John", "Snow");
+        var user2 = new Student("Robert", "Stark");
+
+        var camera1 = new Camera("nikon");
+        var laptop1 = new Laptop("mac");
+        var projector1 = new Projector("cool");
+        
+        var rs = new RentalService();
+        rs.AddUser(user1);
+        rs.AddUser(user2);
+        
+        rs.AddEquipment(camera1);
+        rs.AddEquipment(laptop1);
+        rs.AddEquipment(projector1);
+        
+        rs.DisplayAllEquipment();
+        rs.DisplayAvailableEquipment();
+        
+        rs.RentToUser();
+        rs.ReturnEquipment();
+        
+        rs.ChangeEquipmentAvailability();
+        
+        rs.DisplayUserRentals();
+        
+        rs.DisplayOverdueRentals();
+        
+        rs.GenerateShortSummary()
+        
+
+    }
+}
